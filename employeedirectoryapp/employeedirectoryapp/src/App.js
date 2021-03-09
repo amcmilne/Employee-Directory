78a7196b512e6365131e561f0e775a0ad1283630
amcmilne <amcmilne@gmail.com>
2021-03-08 20:17:06 -0500

    deploy

---
+++ employeedirectoryapp/employeedirectoryapp/src/App.js
@@ -0,0 +1,12 @@
+import EmployeeContainer from "../src/components/EmployeeContainer/index.js";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <EmployeeContainer />  
+    </div>
+  );
+}
+
+export default App;
