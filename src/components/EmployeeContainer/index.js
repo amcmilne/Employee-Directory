f94e4d915a313b93c9aaf824b9afeb00e24a0575
amcmilne <amcmilne@gmail.com>
2021-03-08 22:44:43 -0500

    working app

---
+++ src/components/EmployeeContainer/index.js
@@ -0,0 +1,99 @@
+import React from "react";
+import Wrapper from "../Wrapper/index.js";
+import Header from "../Header/index.js";
+import EmployeeResults from "../EmployeeResults/index.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import API from "../../utils/API.js";
+
+class EmployeeContainer extends React.Component {
+  state = {
+    search: "",
+    employees: [],
+    filteredEmployees: [],
+    order: "",
+  };
+
+  //initial API call on page load
+  componentDidMount() {
+    API.search()
+      .then((res) =>
+        this.setState({
+          employees: res.data.results,
+          filteredEmployees: res.data.results,
+        })
+      )
+      .catch((err) => console.log(err));
+  }
+
+  //sort in ascending or descending order
+  sortByName = () => {
+    const filtered = this.state.filteredEmployees;
+    if (this.state.order === "asc") {
+      const sorted = filtered.sort((a, b) =>
+        a.name.last > b.name.last ? 1 : -1
+      );
+      console.log(sorted);
+
+      this.setState({
+        filteredEmployees: sorted,
+        order: "desc",
+      });
+    } else {
+      const sorted = filtered.sort((a, b) =>
+        a.name.last > b.name.last ? -1 : 1
+      );
+      console.log(sorted);
+
+      this.setState({
+        filteredEmployees: sorted,
+        order: "asc",
+      });
+    }
+  };
+
+  //handle dynamic loading
+  handleInputChange = (e) => {
+    const employees = this.state.employees;
+    const UserInput = e.target.value;
+    const filteredEmployees = employees.filter(
+      (employee) =>
+        employee.name.first.toLowerCase().indexOf(UserInput.toLowerCase()) > -1
+    );
+    this.setState({
+      filteredEmployees,
+    });
+  };
+  
+  //API call for secondary searches
+  employeeSearch = () => {
+    API.search()
+      .then((res) =>
+        this.setState({
+          filteredEmployees: res.data.results,
+          employees: res.data.results,
+        })
+      )
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <div>
+        <Wrapper>
+          <Header
+            handleInputChange={this.handleInputChange}
+            handleFormSubmit={this.handleFormSubmit}
+            employees={this.state.employees}
+            results={this.state.filteredEmployees} />      
+          <EmployeeResults
+            results={this.state.filteredEmployees}
+            sortByName={this.sortByName}
+          />
+        </Wrapper>
+      </div>
+    );
+  }
+}
+
+export default EmployeeContainer;
---
+++ src/components/NameSearch/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NameSearch(props) {
+  return (
+    <div className="container-fluid">
+      <form className="d-flex" />
+      <input
+        onChange={props.handleInputChange}
+        className="form-control me-2"
+        name="search"
+        type="text"
+        placeholder="Quick Search by Name"
+      />
+    </div>
+  );
+}
+export default NameSearch;
